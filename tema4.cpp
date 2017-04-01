393fc412e608bc856ec88209c75a786f38eb00f3
tabby336 <ioana.bogdan25@gmail.com>
2017-04-01 17:21:07 +0300

    MOARTEA PASINII MELE

--- tema4.cpp
+++ tema4.cpp
@@ -19,10 +19,6 @@ using namespace std;
 #define MY_JF 0.005
 
 unsigned char prevKey;
-unsigned char last_prevKey;
-int nivel = 0;
-
-
 
 class GrilaCarteziana {
 public:
@@ -52,6 +48,11 @@ public:
     }
     
     void adauga_pixel(int i, int j) {
+        if(i > this->linii || i < 0 )
+            return;
+        if(j > this->coloane || j < 0)
+            return;
+        
         float _i = -0.8 + distanta_linie*i;
         float _j = -0.8 + distanta_coloana*j;
         
@@ -69,6 +70,28 @@ public:
         glEnd();
     }
     
+    void print_grosime_verticala(int i,int j,int grosime) {
+        int upper = j+grosime/2; ;
+        if(!(grosime%2)) {
+            upper--;
+        }
+        for(int k = j - grosime/2; k <= upper; ++k) {
+            adauga_pixel(i, k);
+        }
+    }
+    
+    void print_grosime_orizontala(int i, int j, int grosime) {
+        int upper = i+grosime/2; ;
+        if(!(grosime%2)) {
+            upper--;
+        }
+        for(int k = i - grosime/2; k <= upper; ++k) {
+            adauga_pixel(k, j);
+        }
+
+        
+    }
+    
     void deseneaza_dreapta(pair<int, int> x, pair<int,int> y) {
         glColor3f(0.1, 0.1, 0.1);
         glLineWidth(3);
@@ -78,83 +101,128 @@ public:
         glEnd();
     }
     
+    
+    void AfisareSegmentDreapta3(int x0, int y0, int xn, int yn, int grosime = 1) {
+        // ordonare dupa x
+        int aux;
+        if (x0 > xn) {
+            aux = x0;
+            x0 = xn;
+            xn = aux;
+            
+            aux = y0;
+            y0= yn;
+            yn = aux;
+        }
+
+        int x = x0, y = y0;
+        
+        // panta
+        double m = float(yn - y0)/(xn-x0);
+        int dx = xn - x0;
+        
+        if(y0 <  yn) {
+            int dy = yn - y0;
+            
+            int dE = 2*dy;
+            int dNE = 2*(dy-dx);
+            int dN = -2*dx;
+            
+            if( fabs(m) < 1) { //panta < 1
+                int d = 2*dy - dx;
+                print_grosime_verticala(x, y, grosime);
+                while(x < xn) {
+                    if(d<0) {
+                        d += dE;
+                        x++;
+                    }else {
+                        d += dNE;
+                        x++;
+                        y++;
+                    }
+                    print_grosime_verticala(x,y,grosime);
+                }
+            } else { //panta > 1
+                int d = 2*dx - dy;
+                print_grosime_orizontala(x,y,grosime);
+                while(y < yn) {
+                    if(d<0) {
+                        d += dNE;
+                        y++;
+                        x++;
+                    } else {
+                        d += dN;
+                        y++;
+                        
+                    }
+                    print_grosime_orizontala(x,y,grosime);
+                }
+            }
+        } else { // y0 > yn
+            
+            
+            int dy = y0 - yn;
+            
+            int dE = 2*dy;
+            int dNE = 2*(dy-dx);
+            int dS = 2*dx;
+            int dSE = -dNE;
+
+            if( fabs(m) < 1) { //panta < 1
+                int d = 2*dy - dx;
+                print_grosime_verticala(x,y, grosime);
+                while ( x < xn ) {
+                    if(d < 0) {
+                        d += dE;
+                        x++;
+                    } else {
+                        d += dNE;
+                        y--;
+                        x++;
+                    }
+                    print_grosime_verticala(x,y, grosime);
+                }
+            } else { //panta < 1
+                int dx = xn - x0;
+                int dy = y0 - yn;
+                int d = 2*dx - dy;
+                
+                print_grosime_orizontala(x,y,grosime);
+                while ( y > yn ) {
+                    if(d < 0) {
+                        d += dS;
+                        y--;
+                    } else {
+                        d += dSE;
+                        x++;
+                        y--;
+                    }
+                    print_grosime_orizontala(x,y,grosime);
+                }
+            }
+        }
+    }
+    
+
+    
 private:
     int linii, coloane;
     float distanta_linie, distanta_coloana;
 };
 
-
-vector<pair<int, int>> AfisareSegmentDreapta3(int x0, int y0, int xn, int yn) {
-    double m = float(yn - y0)/(xn-x0);
-    vector<pair<int,int>> M(100);
-    
-    if((x0 < xn) && (y0 <  yn)) {
-        int dx = xn - x0;
-        int dy = yn - y0;
-        
-        
-        int dE = 2*dy;
-        int dNE = 2*(dy-dx);
-        
-        int dN = -2*dx;
-        
-        int x = x0, y = y0;
-        
-        
-        M.push_back(make_pair(x,y));
-        if( fabs(m) < 1) {
-            int d = 2*dy - dx;
-            while(x < xn) {
-                if(d<0) {
-                    d += dE;
-                    x++;
-                }else {
-                    d += dNE;
-                    x++;
-                    y++;
-                }
-                M.push_back(make_pair(x,y));
-            }
-        } else {
-            int d = 2*dx - dy;
-            while(y < yn) {
-                if(d<0) {
-                    d += dNE;
-                    y++;
-                    x++;
-                } else {
-                    d += dN;
-                    y++;
-                    
-                }
-                M.push_back(make_pair(x,y));
-            }
-        }
-    } else {
-        //
-    }
-    return M;
-    
-}
-
 // abs(panta < 1) si x0 < xn si y0 < yn
 void Display1() {
     pair<int, int> x0 = make_pair(0,0);
     pair<int, int> y0 = make_pair(15,7);
+    int grosime = 4;
     
     GrilaCarteziana gc(15, 15);
     glPushMatrix();
     glLoadIdentity();
     gc.deseneaza();
-    gc.adauga_pixel(x0.first, x0.second);
-    gc.adauga_pixel(y0.first, y0.second);
     gc.deseneaza_dreapta(x0, y0);
-    
-    
-    vector<pair<int, int>> M = AfisareSegmentDreapta3(x0.first, x0.second, y0.first, y0.second);
-    for(vector<pair<int,int>>::iterator it = M.begin(); it != M.end(); ++it) {
-        gc.adauga_pixel(it->first, it->second);
-    }
+    gc.AfisareSegmentDreapta3(x0.first, x0.second, y0.first, y0.second, grosime);
+
     glPopMatrix();
 }
 
@@ -162,26 +230,49 @@ void Display1() {
 void Display2() {
     pair<int, int> x0 = make_pair(0,0);
     pair<int, int> y0 = make_pair(7,15);
+    int grosime = 2;
     
     GrilaCarteziana gc(15, 15);
     glPushMatrix();
-    //glLoadIdentity();
+    glLoadIdentity();
     gc.deseneaza();
-    gc.adauga_pixel(x0.first, x0.second);
-    
-    gc.adauga_pixel(0, 1);
-    gc.adauga_pixel(y0.first, y0.second);
+    gc.AfisareSegmentDreapta3(x0.first, x0.second, y0.first, y0.second, grosime);
+
     
     gc.deseneaza_dreapta(x0, y0);
-    
-    
-    vector<pair<int, int>> M = AfisareSegmentDreapta3(x0.first, x0.second, y0.first, y0.second);
-    for(vector<pair<int,int>>::iterator it = M.begin(); it != M.end(); ++it) {
-        gc.adauga_pixel(it->first, it->second);
-    }
     glPopMatrix();
 }
 
+void Display3() {
+    pair<int, int> x0 = make_pair(0,15);
+    pair<int, int> y0 = make_pair(15,10);
+    int grosime = 3;
+    
+    GrilaCarteziana gc(15, 15);
+    glPushMatrix();
+    glLoadIdentity();
+    gc.deseneaza();
+//    gc.print_grosime(x0.first, x0.second,grosime);
+    gc.AfisareSegmentDreapta3(x0.first, x0.second, y0.first, y0.second, grosime);
+
+    gc.deseneaza_dreapta(x0, y0);
+    glPopMatrix();
+}
+
+
+void Display4() {
+    pair<int, int> x0 = make_pair(0,15);
+    pair<int, int> y0 = make_pair(6,0);
+    
+    GrilaCarteziana gc(15, 15);
+    glPushMatrix();
+    glLoadIdentity();
+    gc.deseneaza();
+    gc.AfisareSegmentDreapta3(x0.first, x0.second, y0.first, y0.second);
+    
+    gc.deseneaza_dreapta(x0, y0);
+    glPopMatrix();
+}
 
 void Init(void) {
     
@@ -196,15 +287,20 @@ void Init(void) {
 
 void Display(void)
 {
+    glClear(GL_COLOR_BUFFER_BIT);
     switch(prevKey)
     {
         case '1':
-            glClear(GL_COLOR_BUFFER_BIT);
             Display1();
             break;
         case '2':
-            glClear(GL_COLOR_BUFFER_BIT);
             Display2();
+            break;
+        case '3':
+            Display3();
+            break;
+        case '4':
+            Display4();
             break;
         default:
             break;
@@ -255,6 +351,3 @@ int main(int argc, char** argv)
     
     return 0;
 }
-
-
-
